6a0c0a9634c35a7157db90b2a5835155e0bec344
Angell <angellnathaline0605@gmail.com>
2024-11-18 21:17:56 +0800

    Refactor: Make Car and PriceAverage private inner classes.

--- CarImport.java
+++ CarImport.java
@@ -1,24 +1,36 @@
 import java.util.Scanner; // Import the Scanner class
 
-class Car {
-    String model;
-    String make;
-    double price;
-
-    public Car(String model, String make, double price) {
-        this.model = model;
-        this.make = make;
-        this.price = price;
-    }
-}
-
 public class CarImport {
+    private static class Car { 
+        String model;
+        String make;
+        double price;
+
+        public Car(String model, String make, double price) {
+            this.model = model;
+            this.make = make;
+            this.price = price;
+        }
+    }
+
+    private static class PriceAverage { 
+        public static double calculateAveragePrice(Car[] cars) {
+            double totalPrice = 0.0;
+
+            for (Car car : cars) {
+                totalPrice += car.price;
+            }
+
+            return totalPrice / cars.length;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Car[] cars = new Car[3]; 
+        Car[] cars = new Car[3];
 
-        for (int i = 0; i < cars.length; i++) { 
-            System.out.println("\nEnter details for car " + (i + 1) + ": " );
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println("\nEnter details for car " + (i + 1) + ": ");
 
             System.out.print("Enter model: ");
             String enteredModel = scanner.nextLine();
@@ -28,24 +40,12 @@ public class CarImport {
 
             System.out.print("Enter price: ");
             double enteredPrice = scanner.nextDouble();
-            scanner.nextLine(); 
+            scanner.nextLine();
 
             cars[i] = new Car(enteredModel, enteredMake, enteredPrice);
         }
 
         double averagePrice = PriceAverage.calculateAveragePrice(cars);
         System.out.println("\nThe average price of the cars is: " + averagePrice);
-    }
-}
-
-class PriceAverage {
-    public static double calculateAveragePrice(Car[] cars) {
-        double totalPrice = 0.0;
-
-        for (Car car : cars) {
-            totalPrice += car.price;
-        }
-
-        return totalPrice / cars.length;
     }
 }
