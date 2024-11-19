93b3caa067c1bc869f5c964f19fd5163e9c75bdd
Angell <angellnathaline0605@gmail.com>
2024-11-19 11:59:11 +0800

    Assignment 3, modify program as question needed

--- SwitchCar.java
+++ SwitchCar.java
@@ -4,20 +4,28 @@ public class SwitchCar {
   public static void main (String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Enter the car model: ");
-    String enteredModel = scanner.nextLine();
+    System.out.println("Please select the car model that you want to buy: [1] Honda , [2] Proton , [3] Perodua");
+    int enteredModel = scanner.nextInt();
 
-    System.out.println("Enter the car's price (RM): ");
-    double enteredPrice = scanner.nextDouble();
-
-    int buyerStatus = (enteredPrice > 100000) ? 1 : 2;
-    switch (buyerStatus) {
+    double carPrice = 0;
+    switch (enteredModel) {
       case 1:
+        System.out.println("Car Model: Honda");
+        carPrice = 200000;
+        System.out.println("Car's Price: RM " + carPrice);
         System.out.println("The buyer is a VIP!");
         break;
       case 2:
+        System.out.println("Car Model: Proton");
+        carPrice = 95000;
+        System.out.println("Car's Price: RM " + carPrice);
         System.out.println("The buyer is not a VIP!");
         break;
+      case 3:
+        System.out.println("Car Model: Perodua");
+        carPrice = 100000;
+        System.out.println("Car's Price: RM " + carPrice);
+        System.out.println("The buyer is not a VIP!");
       default:
         System.out.println("Invalid!");
         break;
